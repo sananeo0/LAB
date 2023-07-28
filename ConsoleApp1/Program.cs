ddb8fb5f0143016a134ede603f54fb17c61ee761
sananeo0 <sananeo@code.edu.az>
2023-07-28 21:38:08 +0400

    else if

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace NumberGuessingGame
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Random random = new Random();
+                int targetNumber = random.Next(1, 101);
+
+                Console.WriteLine("1 ile 100 arasında bir sayı tuttum. Hadi tahmin et!");
+
+                while (true)
+                {
+                    Console.Write("Tahmininizi girin: ");
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out int guess))
+                    {
+                        Console.WriteLine("Geçersiz giriş! Lütfen bir sayı girin.");
+                        continue;
+                    }
+
+                    if (guess < 1 || guess > 100)
+                    {
+                        Console.WriteLine("Lütfen 1 ile 100 arasında bir sayı girin.");
+                    }
+                    else if (guess < targetNumber)
+                    {
+                        Console.WriteLine("Daha yüksek bir sayı tahmin edin.");
+                    }
+                    else if (guess > targetNumber)
+                    {
+                        Console.WriteLine("Daha düşük bir sayı tahmin edin.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tebrikler! Doğru sayıyı tahmin ettiniz.");
+                        break; // Doğru cevabı bulduk, içteki döngüyü sonlandırın.
+                    }
+                }
+
+                Console.Write("Yeniden oynamak istiyor musunuz? (E/H): ");
+                string playAgainInput = Console.ReadLine().Trim().ToLower();
+
+                if (playAgainInput != "e")
+                {
+                    break; // Dıştaki döngüyü sonlandırın ve oyunu kapatın.
+                }
+            }
+
+            Console.WriteLine("Oyun bitti. İyi günler!");
         }
     }
 }
